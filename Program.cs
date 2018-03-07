633afd901988daec8304d5a1c5157e8c429b43ca
Risto Matikainen <risto.matikainen@4straction.com>
2018-03-07 17:34:06 +0200

    STR-2030 - General pipedrive implementation for all customers
    - Swagger compatible API and some tests that API works

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using IO.Swagger.Client;
+using IO.Swagger.Api;
+using IO.Swagger.Model;
+
 namespace pipedrive_to_4straction
 {
     class Program
@@ -39,6 +43,38 @@ namespace pipedrive_to_4straction
              * Settings (app.config)
              * 4straction credentials
              */
+
+            Configuration config = new Configuration();
+
+            //https://4straction.io/v1/CompanyFunction/loadAll?4S-API-User-ID=risto.matikainen@4straction.com&4S-API-User-Password=Kolmendal&4S-API-Partner-Key=65180b15884bf62abf91e6d586504af7
+
+            /*  */
+            config.AddDefaultHeader("4S-API-User-ID", "risto.matikainen@4straction.com");
+            config.AddDefaultHeader("4S-API-User-Password", "K0lmendal");
+            config.AddDefaultHeader("4S-API-Partner-Key", "65180b15884bf62abf91e6d586504af7");
+            config.AddDefaultHeader("4S-API-Organization-Identifier-Override", "2700787-8");
+
+            // should we expose business id to company report?
+
+            UserCompanyAccessApi accesses = new UserCompanyAccessApi(config);
+            foreach (UserCompanyAccessReport access in accesses.UserCompanyAccessLoadMyAccessGet(""))
+            {
+                Console.WriteLine(access.CompanyName);
+            }
+
+            // client id?!
+            // http methods!?
+            // indicatorids is just "body" here. Not really helpful
+            // mergeIndicatordata not available in swagger
+
+            PerformanceIndicatorMonthDataApi months = new PerformanceIndicatorMonthDataApi(config);
+            List<int?> indicators = new List<int?>();
+            indicators.Add(208);
+
+            foreach (PerformanceIndicatorMonthData data in months.PerformanceIndicatorMonthDataLoadCompetitorValuesForYearPut("", indicators, 2017, 30))
+            {
+                Console.WriteLine(data.ID);
+            }
 
 
         }
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # pipedrive-to-4straction
 General component that integrates pipedrive sales pipeline to 4straction management decision making
+
+How this formatting works?
+
+Parts in this implementation:
+
+1. 4Straction API implementation using swagger compatible client API
+2. Pipedrive API implementation using modelled classes
+3. Organize and map Pipedrive pipelines and stages into usable items
+4. Push needed information to 4straction
+
+
+Other stuff:
+
+- How to update 4straction client when API changes:
+
+java -jar swagger-codegen-cli-2.2.1.jar generate  -i 4straction.json -l csharp
+
+=> Then limited entities to a subset..
